3a248aaaf8bfecc88f324ad1f88b65f1a74684e9
Allyson Holdahl <allyson.holdahl@gmail.com>
2025-06-02 18:17:00 -0500

    base backend with login

---
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -0,0 +1,18 @@
+package dbrepo
+
+import (
+	"backend/internal/models"
+	"context"
+	"database/sql"
+	"time"
+)
+
+type PostgresDBRepo struct {
+	DB *sql.DB
+}
+
+const dbTimeout = time.Second * 3
+
+func (m *PostgresDBRepo) Connection() *sql.DB {
+	return m.DB
+}
